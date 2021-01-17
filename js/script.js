8056e8be9a357c1384084f57558efb80e6e54125
hkhayat786 <73578233+hkhayat786@users.noreply.github.com>
2021-01-17 08:40:29 +0530

    final portfolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+     $  (".aeroplane").animate({
+            "opacity": "0",
+            "position" : "absolute",
+            "left": "10px",
+            "top":"200px",
+        },6000);
+   
+    });
